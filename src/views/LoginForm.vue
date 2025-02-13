2ae8c5f39bed106056b2aff570ab74694c3196b1
hiepit2k3 <phamsonghiepb3k46@gmail.com>
2025-02-13 19:09:00 +0700

    test ci/cd

--- src/views/LoginForm.vue
+++ src/views/LoginForm.vue
@@ -45,7 +45,7 @@ export default {
     async handleLogin() {
       try {
         await this.$store.dispatch('login', { email: this.email, password: this.password });
-        alert("Đăng nhập thành công!");
+        console.log("dsgfhsdjfhsajkdh");
         this.$router.push("/");  // Chuyển hướng về trang chủ
       } catch (error) {
         alert("Đăng nhập thất bại!");
